62579009323d0d034eecb0388af0f907d98e168d
RafaelH31 <rafael.cebrian@sptech.school>
2024-06-10 00:01:01 -0300

    feat: dashnoard funcional

--- js/dash_aluno.js
+++ js/dash_aluno.js
@@ -21,76 +21,76 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    const courseSelect = document.getElementById('course-select');
+    // const courseSelect = document.getElementById('course-select');
 
-    const statsData = {
-        excel: {
-            atividadesEntregues: '45%',
-            atividadesEntreguesChange: '⬆',
-            pontosSemana: '150 pts',
-            pontosSemanaChange: '⬆',
-            semanaPassadaChange: '+ ⬆ 1K pts'
-        },
-        confeitaria: {
-            atividadesEntregues: '30%',
-            atividadesEntreguesChange: '⬇',
-            pontosSemana: '0 pts',
-            pontosSemanaChange: '⬇',
-            semanaPassadaChange: '+ ⬆ 2K pts'
-        },
-        design: {
-            atividadesEntregues: '50%',
-            atividadesEntreguesChange: '⬆',
-            pontosSemana: '200 pts',
-            pontosSemanaChange: '⬆',
-            semanaPassadaChange: '+ ⬆ 3K pts'
-        }
+    // const statsData = {
+    //     excel: {
+    //         atividadesEntregues: '45%',
+    //         atividadesEntreguesChange: '⬆',
+    //         pontosSemana: '150 pts',
+    //         pontosSemanaChange: '⬆',
+    //         semanaPassadaChange: '+ ⬆ 1K pts'
+    //     },
+    //     confeitaria: {
+    //         atividadesEntregues: '30%',
+    //         atividadesEntreguesChange: '⬇',
+    //         pontosSemana: '0 pts',
+    //         pontosSemanaChange: '⬇',
+    //         semanaPassadaChange: '+ ⬆ 2K pts'
+    //     },
+    //     design: {
+    //         atividadesEntregues: '50%',
+    //         atividadesEntreguesChange: '⬆',
+    //         pontosSemana: '200 pts',
+    //         pontosSemanaChange: '⬆',
+    //         semanaPassadaChange: '+ ⬆ 3K pts'
+    //     }
 
     
-    };
+    // };
 
    
 
-    courseSelect.addEventListener('change', (event) => {
-        const selectedCourse = event.target.value;
-        const data = statsData[selectedCourse];
+    // courseSelect.addEventListener('change', (event) => {
+    //     const selectedCourse = event.target.value;
+    //     const data = statsData[selectedCourse];
 
-        document.getElementById('atividades-entregues').textContent = data.atividadesEntregues;
-        document.getElementById('atividades-entregues-change').textContent = data.atividadesEntreguesChange;
-        document.getElementById('pontos-semana').textContent = data.pontosSemana;
-        document.getElementById('pontos-semana-change').textContent = data.pontosSemanaChange;
-        document.getElementById('semana-passada-change').textContent = data.semanaPassadaChange;
+    //     document.getElementById('atividades-entregues').textContent = data.atividadesEntregues;
+    //     document.getElementById('atividades-entregues-change').textContent = data.atividadesEntreguesChange;
+    //     document.getElementById('pontos-semana').textContent = data.pontosSemana;
+    //     document.getElementById('pontos-semana-change').textContent = data.pontosSemanaChange;
+    //     document.getElementById('semana-passada-change').textContent = data.semanaPassadaChange;
 
-        // Atualizar a imagem da medalha de acordo com o curso selecionado
-        const medalhaCurso = document.getElementById('medalha-curso');
-        if (selectedCourse === 'excel') {
-            medalhaCurso.src = 'img/silver medal.png';
-        } else if (selectedCourse === 'confeitaria') {
-            medalhaCurso.src = 'img/Group 46.png';
-        } else if (selectedCourse === 'design') {
-            medalhaCurso.src = 'img/bronze medal.png';
-        }
-        const atividadesEntreguesChange = document.getElementById('atividades-entregues-change');
-        const pontosSemanaChange = document.getElementById('pontos-semana-change');
-        const semanaPassadaChange = document.getElementById('semana-passada-change');
+    //     // Atualizar a imagem da medalha de acordo com o curso selecionado
+    //     const medalhaCurso = document.getElementById('medalha-curso');
+    //     if (selectedCourse === 'excel') {
+    //         medalhaCurso.src = 'img/silver medal.png';
+    //     } else if (selectedCourse === 'confeitaria') {
+    //         medalhaCurso.src = 'img/Group 46.png';
+    //     } else if (selectedCourse === 'design') {
+    //         medalhaCurso.src = 'img/bronze medal.png';
+    //     }
+    //     const atividadesEntreguesChange = document.getElementById('atividades-entregues-change');
+    //     const pontosSemanaChange = document.getElementById('pontos-semana-change');
+    //     const semanaPassadaChange = document.getElementById('semana-passada-change');
 
-         // Função para definir a cor do texto com base no símbolo de seta
-         const setColor = (element, symbol) => {
-            if (symbol === '⬆' || symbol === '+') {
-                element.classList.remove('red');
-                element.classList.add('green');
-            } else if (symbol === '⬇' || symbol === '-') {
-                element.classList.remove('green');
-                element.classList.add('red');
-            }
-        };
+    //      // Função para definir a cor do texto com base no símbolo de seta
+    //      const setColor = (element, symbol) => {
+    //         if (symbol === '⬆' || symbol === '+') {
+    //             element.classList.remove('red');
+    //             element.classList.add('green');
+    //         } else if (symbol === '⬇' || symbol === '-') {
+    //             element.classList.remove('green');
+    //             element.classList.add('red');
+    //         }
+    //     };
 
-        setColor(atividadesEntreguesChange, data.atividadesEntreguesChange);
-        setColor(pontosSemanaChange, data.pontosSemanaChange);
-        setColor(semanaPassadaChange, data.semanaPassadaChange);
+    //     setColor(atividadesEntreguesChange, data.atividadesEntreguesChange);
+    //     setColor(pontosSemanaChange, data.pontosSemanaChange);
+    //     setColor(semanaPassadaChange, data.semanaPassadaChange);
 
 
-    });
+    // });
 
     $j(document).ready(function() {
         const editProfileButton = document.querySelector('.user-details .btn-primary');
--- js/dash_aluno_api.js
+++ js/dash_aluno_api.js
@@ -2,28 +2,26 @@ document.addEventListener('DOMContentLoaded', async function() {
     const user = JSON.parse(sessionStorage.getItem('user'));
     if (user && user.idUsuario) {
         try {
-          
+            // Fetch atividades do usuario
             const atividadesResponse = await fetch(`http://localhost:8080/grafico/atividadesDoUsuario/${user.idUsuario}`);
             if (!atividadesResponse.ok) {
                 throw new Error('Erro ao buscar dados das atividades');
             }
             const atividadesData = await atividadesResponse.json();
 
-
             let totalAtividades = 0;
             let atividadesFeitas = 0;
             let cursos = [];
 
             atividadesData.forEach(curso => {
-                totalAtividades += curso.totalAtividades;
-                atividadesFeitas += curso.atividadesFeitas;
-                cursos.push(curso.curso);
+                totalAtividades += curso.totalQtdAtividades;
+                atividadesFeitas += curso.totalAtividadesUsuario;
+                cursos.push(curso.nomeCurso);
             });
 
             sessionStorage.setItem('cursos', JSON.stringify(cursos));
 
             const porcentagemFeitas = ((atividadesFeitas / totalAtividades) * 100).toFixed(2);
-
             document.getElementById('atividades-entregues').innerText = `${porcentagemFeitas}%`;
 
             const atividadesChangeElement = document.getElementById('atividades-entregues-change');
@@ -37,40 +35,100 @@ document.addEventListener('DOMContentLoaded', async function() {
                 atividadesChangeElement.innerText = '⬇';
             }
 
-            const pontosResponse = await fetch(`http://localhost:8080/grafico/pontosDaSemana/${user.idUsuario}`);
-            if (!pontosResponse.ok) {
-                throw new Error('Erro ao buscar dados dos pontos da semana');
+            // Fetch pontos ao longo do tempo
+            const graficoLinhaResponse = await fetch(`http://localhost:8080/grafico/pontosAoLongoDoTempo/${user.idUsuario}`);
+            if (!graficoLinhaResponse.ok) {
+                throw new Error('Erro ao buscar dados dos pontos');
             }
-            const pontosData = await pontosResponse.json();
+            const graficoLinhaData = await graficoLinhaResponse.json();
 
-            document.getElementById('pontos-semana').innerText = `${pontosData.pontosSemanaAtual} pts`;
+            // Processando os dados para o Chart.js
+            const cursosGraficoLinha = {};
+            graficoLinhaData.forEach(entry => {
+                const date = new Date(entry.data_pontuacao).toLocaleDateString();
+                if (!cursosGraficoLinha[entry.nome]) {
+                    cursosGraficoLinha[entry.nome] = {};
+                }
+                cursosGraficoLinha[entry.nome][date] = entry.pontos;
+            });
 
-            const pontosChangeElement = document.getElementById('pontos-semana-change');
-            const semanaPassadaChangeElement = document.getElementById('semana-passada-change');
+            const labels = Array.from(new Set(graficoLinhaData.map(entry => new Date(entry.data_pontuacao).toLocaleDateString()))).sort();
+            const datasets = Object.keys(cursosGraficoLinha).map(curso => {
+                return {
+                    label: curso,
+                    data: labels.map(label => cursosGraficoLinha[curso][label] || 0),
+                    fill: false,
+                    borderColor: getRandomColor()
+                };
+            });
 
-            if (pontosData.diferencaPontos >= 0) {
-                pontosChangeElement.classList.remove('red');
-                pontosChangeElement.classList.add('green');
-                pontosChangeElement.innerText = '⬆';
-                semanaPassadaChangeElement.classList.remove('red');
-                semanaPassadaChangeElement.classList.add('green');
-                semanaPassadaChangeElement.innerText = `+ ⬆ ${pontosData.diferencaPontos} pts`;
-            } else {
-                pontosChangeElement.classList.remove('green');
-                pontosChangeElement.classList.add('red');
-                pontosChangeElement.innerText = '⬇';
-                semanaPassadaChangeElement.classList.remove('green');
-                semanaPassadaChangeElement.classList.add('red');
-                semanaPassadaChangeElement.innerText = `${pontosData.diferencaPontos} pts`;
+            // Função para gerar cores aleatórias
+            function getRandomColor() {
+                const letters = '0123456789ABCDEF';
+                let color = '#';
+                for (let i = 0; i < 6; i++) {
+                    color += letters[Math.floor(Math.random() * 16)];
+                }
+                return color;
             }
+
+            // Configurando o gráfico com Chart.js
+            const ctx = document.getElementById('pontosChart').getContext('2d');
+            const myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: labels,
+                    datasets: datasets
+                },
+                options: {
+                    responsive: true,
+                    title: {
+                        display: true,
+                        text: 'Pontos ao Longo do Tempo por Curso'
+                    },
+                    tooltips: {
+                        mode: 'index',
+                        intersect: false,
+                    },
+                    hover: {
+                        mode: 'nearest',
+                        intersect: true
+                    },
+                    scales: {
+                        xAxes: [{
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Data'
+                            }
+                        }],
+                        yAxes: [{
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Pontos'
+                            }
+                        }]
+                    }
+                }
+            });
+
+            // Fetch pontos da semana
+            const pontosSemanaResponse = await fetch(`http://localhost:8080/grafico/pontosDaSemana/${user.idUsuario}`);
+            if (!pontosSemanaResponse.ok) {
+            throw new Error('Erro ao buscar dados dos pontos da semana');
+            }
+            const pontosSemanaData = await pontosSemanaResponse.json();
+
+            document.getElementById('pontos-semana').innerText = `${pontosSemanaData.pontosSemanaAtual}`;
+            document.getElementById('semana-passada-change').innerText = `${pontosSemanaData.diferencaPontos}`;
+
+
 
         } catch (error) {
             console.error('Erro:', error);
-            alert('Erro ao carregar dados');
         }
     } else {
-      
         window.location.href = '/login.html';
     }
 });
-
