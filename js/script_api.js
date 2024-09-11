56bb35bcf8e3d04d003501c8565d035e4ce7333f
brudneyuk <brudney.ramosjr@sptech.school>
2024-09-10 23:29:37 -0300

    fix: Arrumando parte de cadastro, para não deixar cadastrar o endereço primeiro caso estiver algum campo errado, ainda deverá ser feito melhoria para melhor funcionamento

--- js/script_api.js
+++ js/script_api.js
@@ -1,24 +1,39 @@
 async function realizarCadastro() {
+    // Verifica se todos os campos obrigatórios estão preenchidos antes de iniciar o cadastro
+    const todosCamposValidos = validarCampos();
+    if (!todosCamposValidos) {
+        showAlert('error', 'Por favor, preencha todos os campos obrigatórios.');
+        return;
+    }
+
+    // Cadastra o endereço e aguarda a resposta
     const enderecoId = await cadastrarEndereco();
+    if (enderecoId === null) {
+        return; // Se houve erro no cadastro do endereço, não prossegue
+    }
+
     const isRH = window.location.pathname.includes('cadastro_rh');
     const tipoUsuario = isRH ? 3 : 2;
 
-    if (enderecoId !== null) {
-        const id = await cadastrarUsuario(enderecoId);
+    // Cadastra o usuário e aguarda a resposta
+    const id = await cadastrarUsuario(enderecoId);
+    if (id === null) {
+        return; // Se houve erro no cadastro do usuário, não prossegue
+    }
 
-        if (id && tipoUsuario == 3) {
-            await cadastrarDadosEmpresa(id);
-        } else if (id) {
-            showAlert('success', 'Cadastro realizado com sucesso!');
-            setTimeout(() => {
-                window.location.href = "login.html";
-            }, 3000);
-        }
+    // Cadastra dados da empresa se o usuário for do tipo RH
+    if (tipoUsuario === 3) {
+        await cadastrarDadosEmpresa(id);
+    } else {
+        showAlert('success', 'Cadastro realizado com sucesso!');
+        setTimeout(() => {
+            window.location.href = "login.html";
+        }, 3000);
     }
 }
 
-async function cadastrarEndereco() {
-    // Obtendo valores dos campos
+function validarCampos() {
+    // Obtém valores dos campos
     const logradouro = document.getElementById('street').value;
     const numero = document.getElementById('number').value;
     const cidade = document.getElementById('city').value;
@@ -35,15 +50,19 @@ async function cadastrarEndereco() {
     const dtNascInput = document.getElementById('dataNascimento').value;
 
     // Verifica se todos os campos obrigatórios estão preenchidos
-    if (!logradouro || !numero || !cidade || !estado || !cep ||
-        !usernameInput || !emailInput || !passwordInput ||
-        !firstnameInput || !lastnameInput || !cpfInput || 
-        !telefoneInput || !escolaridadeInput || !dtNascInput) {
-        showAlert('error', 'Por favor, preencha todos os campos obrigatórios.');
-        return null;
-    }
+    return logradouro && numero && cidade && estado && cep &&
+        usernameInput && emailInput && passwordInput &&
+        firstnameInput && lastnameInput && cpfInput &&
+        telefoneInput && escolaridadeInput && dtNascInput;
+}
 
-    // Cria o objeto de endereço
+async function cadastrarEndereco() {
+    const logradouro = document.getElementById('street').value;
+    const numero = document.getElementById('number').value;
+    const cidade = document.getElementById('city').value;
+    const estado = document.getElementById('state').value;
+    const cep = document.getElementById('cep').value;
+
     const endereco = {
         cep: cep,
         cidade: cidade,
@@ -62,26 +81,22 @@ async function cadastrarEndereco() {
         });
 
         const data = await response.json();
-        console.log('Resposta do servidor:', data);
-
         if (response.status === 201) {
-            const enderecoId = data.id;
-            console.log('Endereço cadastrado com sucesso! ID:', enderecoId);
-            return enderecoId;
+            return data.id;
         } else {
-            console.error('Erro ao cadastrar endereço', response.status, data.message);
             showAlert('error', `Erro ao cadastrar endereço: ${data.message}`);
             return null;
         }
     } catch (error) {
-        console.error('Erro ao enviar requisição:', error);
         showAlert('error', 'Erro ao tentar cadastrar o endereço');
         return null;
     }
 }
 
-function formatarDataNascimento() {
-    const dataInput = document.getElementById('dataNascimento');
+let timeoutId;
+
+function formatarDataNascimento(event) {
+    const dataInput = event.target;
     let dataFormatada = dataInput.value.replace(/\D/g, ''); // Remove caracteres não numéricos
 
     // Adiciona a máscara de data
@@ -102,6 +117,46 @@ function formatarDataNascimento() {
     if (dataInput.value.length > 10) {
         dataInput.value = dataInput.value.slice(0, 10);
     }
+
+    // Realiza a validação e formatação após um breve atraso
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+        validarDataEAtualizarCampo(dataInput);
+    }, 1000); // Ajuste o atraso conforme necessário
+}
+
+function validarDataEAtualizarCampo(input) {
+    if (!validarData(input.value)) {
+        showAlert('error', 'Data inválida. Por favor, insira uma data válida no formato DD/MM/AAAA.');
+        input.value = ''; // Limpa o campo em caso de erro
+    }
+}
+
+function validarData(data) {
+    // Verifica se a data está no formato DD/MM/AAAA
+    const partes = data.split('/');
+    if (partes.length !== 3) {
+        return false;
+    }
+    const [dia, mes, ano] = partes.map(part => parseInt(part, 10));
+
+    // Verifica se os valores são válidos
+    if (isNaN(dia) || isNaN(mes) || isNaN(ano) || mes < 1 || mes > 12 || dia < 1 || dia > 31) {
+        return false;
+    }
+
+    // Verifica a validade do dia em relação ao mês e ano
+    if ((mes === 4 || mes === 6 || mes === 9 || mes === 11) && dia > 30) {
+        return false;
+    }
+    if (mes === 2) {
+        const isBissexto = (ano % 4 === 0 && (ano % 100 !== 0 || ano % 400 === 0));
+        if ((isBissexto && dia > 29) || (!isBissexto && dia > 28)) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 function formatarDataParaEnvio(data) {
@@ -125,33 +180,41 @@ async function cadastrarUsuario(idEndereco) {
     const telefoneInput = document.getElementById('telefone');
     const escolaridadeInput = document.getElementById('escolaridade');
     const dtNascInput = document.getElementById('dataNascimento');
-    const cepInput = document.getElementById('cep');
-    
 
-    // Validação dos campos obrigatórios
-    if (!usernameInput.value || !emailInput.value || !passwordInput.value ||
-        !firstnameInput.value || !lastnameInput.value || !cpfInput.value ||
-        !telefoneInput.value || !escolaridadeInput.value || !dtNascInput.value ||
-        !cepInput.value) {
-        showAlert('error', 'Por favor, preencha todos os campos obrigatórios.');
-        return null;
+    // Formatar e validar CPF
+    const cpf = cpfInput.value.replace(/\D/g, '');
+    if (!validarCPF(cpf)) {
+        showAlert('error', 'CPF inválido. Por favor, insira um CPF válido.');
+        return;
+    }
+
+    // Formatar e validar telefone
+    const telefone = telefoneInput.value.replace(/\D/g, '');
+    if (!validarTelefone(telefone)) {
+        showAlert('error', 'Telefone inválido. Por favor, insira um telefone válido.');
+        return;
+    }
+
+    // Validar a data de nascimento antes de formatar
+    const dataNascimento = formatarDataParaEnvio(dtNascInput.value);
+    if (!dataNascimento) {
+        showAlert('error', 'Data de nascimento inválida. Por favor, insira uma data válida.');
+        return;
     }
 
     const usuario = {
         nomeUsuario: usernameInput.value,
-        cpf: cpfInput.value.replace(/\D/g, ''),
+        cpf: cpf, // CPF sem pontuações
         senha: passwordInput.value,
         primeiroNome: firstnameInput.value,
         sobrenome: lastnameInput.value,
         email: emailInput.value,
-        telefone: telefoneInput.value,
+        telefone: telefone, // Telefone sem pontuações
         tipoUsuario: document.getElementById('is-rh').checked ? 2 : 1,
         escolaridade: escolaridadeInput.value,
         enderecoId: idEndereco,
-        dtNasc: formatarDataParaEnvio(dtNascInput.value)
+        dtNasc: dataNascimento // Data de nascimento formatada
     };
-
-    console.log('Dados do usuário:', JSON.stringify(usuario));
 
     try {
         const response = await fetch('http://localhost:8080/usuarios/cadastro', {
@@ -160,32 +223,16 @@ async function cadastrarUsuario(idEndereco) {
             body: JSON.stringify(usuario)
         });
 
-        if (!response.ok) {
-            // Se a resposta não for ok, exiba uma mensagem genérica
-            const errorData = await response.json();
-            const errorMessage = errorData.message || 'Erro ao realizar cadastro';
-            console.error('Erro ao realizar cadastro', response.status, errorMessage);
-            showAlert('error', errorMessage);
-            return null;
-        }
-
-        const data = await response.json();
-        console.log('Resposta do servidor:', data);
-
-        if (response.status === 201) {
-            console.log('Cadastro bem sucedido');
+        if (response.ok) {
+            const data = await response.json();
             showAlert('success', 'Cadastro realizado com sucesso!');
-            setTimeout(() => {
-                window.location.href = "login.html";
-            }, 3000);
             return data.idUsuario;
         } else {
-            console.error('Erro inesperado', response.status);
-            showAlert('error', 'Erro ao realizar cadastro');
+            const errorData = await response.json();
+            showAlert('error', errorData.message || 'Erro ao realizar cadastro');
             return null;
         }
     } catch (error) {
-        console.error('Erro:', error);
         showAlert('error', 'Erro ao tentar fazer cadastro');
         return null;
     }
@@ -207,21 +254,68 @@ document.getElementById('cep').addEventListener('input', function () {
     buscarCep();
 });
 
-function formatarCpf() {
-    const cpfFormatar = document.getElementById('cpf');
-    var cpfFormatado = cpfFormatar.value.replace(/\D/g, '');
+function formatarCPF(event) {
+    const cpfInput = event.target;
+    let cpfFormatado = cpfInput.value.replace(/\D/g, ''); // Remove caracteres não numéricos
 
+    // Adiciona a máscara de CPF
     if (cpfFormatado.length <= 3) {
         cpfFormatado = cpfFormatado.replace(/(\d{1,3})/, '$1');
     } else if (cpfFormatado.length <= 6) {
         cpfFormatado = cpfFormatado.replace(/(\d{3})(\d{1,3})/, '$1.$2');
     } else if (cpfFormatado.length <= 9) {
         cpfFormatado = cpfFormatado.replace(/(\d{3})(\d{3})(\d{1,3})/, '$1.$2.$3');
-    } else if (cpfFormatado.length <= 11) {
+    } else {
         cpfFormatado = cpfFormatado.replace(/(\d{3})(\d{3})(\d{3})(\d{1,2})/, '$1.$2.$3-$4');
     }
 
-    cpfFormatar.value = cpfFormatado;
+    // Atualiza o valor do input com a formatação aplicada
+    cpfInput.value = cpfFormatado;
+
+    // Limita o comprimento do campo
+    if (cpfInput.value.length > 14) {
+        cpfInput.value = cpfInput.value.slice(0, 14);
+    }
+
+    // Valida o CPF formatado
+    if (!validarCPF(cpfInput.value)) {
+        showAlert('error', 'CPF inválido. Por favor, insira um CPF válido.');
+    }else {
+        showAlert("sucess","Cpf válido. Sucesso, Cpf inserido é válido")
+    }
+}
+
+function validarCPF(cpf) {
+    // Remove caracteres não numéricos
+    const cpfNumeros = cpf.replace(/\D/g, '');
+
+    // Verifica se tem 11 dígitos
+    if (cpfNumeros.length !== 11 || /^(\d)\1{10}$/.test(cpfNumeros)) {
+        return false;
+    }
+
+    // Valida os dígitos verificadores
+    let soma = 0;
+    let digito;
+    
+    // Validação do primeiro dígito
+    for (let i = 0; i < 9; i++) {
+        soma += cpfNumeros[i] * (10 - i);
+    }
+    digito = (soma * 10) % 11;
+    if (digito === 10 || digito === 11) digito = 0;
+    if (digito !== parseInt(cpfNumeros[9], 10)) return false;
+
+    // Validação do segundo dígito
+    soma = 0;
+    for (let i = 0; i < 10; i++) {
+        soma += cpfNumeros[i] * (11 - i);
+    }
+    digito = (soma * 10) % 11;
+    if (digito === 10 || digito === 11) digito = 0;
+    if (digito !== parseInt(cpfNumeros[10], 10)) return false;
+
+    return true;
 }
 
 function formatarCnpj() {
@@ -243,21 +337,41 @@ function formatarCnpj() {
     cnpjFormatar.value = cnpjFormatado;
 }
 
-function formatarTelefone() {
-    const telefoneFormatar = document.getElementById('telefone');
-    let telefoneFormatado = telefoneFormatar.value.replace(/\D/g, '');
+function formatarTelefone(event) {
+    const telefoneInput = event.target;
+    let telefoneFormatado = telefoneInput.value.replace(/\D/g, ''); // Remove caracteres não numéricos
 
+    // Adiciona a máscara de telefone
     if (telefoneFormatado.length <= 2) {
         telefoneFormatado = telefoneFormatado.replace(/(\d{1,2})/, '($1');
-    } else if (telefoneFormatado.length <= 6) {
-        telefoneFormatado = telefoneFormatado.replace(/(\d{2})(\d{1,4})/, '($1)$2');
-    } else if (telefoneFormatado.length <= 10) {
-        telefoneFormatado = telefoneFormatado.replace(/(\d{2})(\d{4})(\d{0,4})/, '($1)$2-$3');
-    } else if (telefoneFormatado.length <= 11) {
-        telefoneFormatado = telefoneFormatado.replace(/(\d{2})(\d{5})(\d{0,4})/, '($1)$2-$3');
+    } else if (telefoneFormatado.length <= 7) {
+        telefoneFormatado = telefoneFormatado.replace(/(\d{2})(\d{1,5})/, '($1) $2');
+    } else {
+        telefoneFormatado = telefoneFormatado.replace(/(\d{2})(\d{5})(\d{1,4})/, '($1) $2-$3');
     }
 
-    telefoneFormatar.value = telefoneFormatado;
+    // Atualiza o valor do input com a formatação aplicada
+    telefoneInput.value = telefoneFormatado;
+
+    // Limita o comprimento do campo
+    if (telefoneInput.value.length > 15) {
+        telefoneInput.value = telefoneInput.value.slice(0, 15);
+    }
+
+    // Valida o telefone formatado
+    if (!validarTelefone(telefoneInput.value)) {
+        showAlert('error', 'Telefone inválido. Por favor, insira um telefone válido no formato (XX) XXXXX-XXXX.');
+    } else {
+        showAlert("sucess", "Telefone válido. Sucesso, o telefone foi inserido em um formato válido!")
+    }
+}
+
+function validarTelefone(telefone) {
+    // Remove caracteres não numéricos
+    const telefoneNumeros = telefone.replace(/\D/g, '');
+
+    // Verifica se o telefone tem 10 ou 11 dígitos
+    return telefoneNumeros.length === 10 || telefoneNumeros.length === 11;
 }
 
 function buscarCep() {
@@ -308,7 +422,7 @@ function showAlert(type, message) {
         alertContainer.classList.remove('show');
         setTimeout(() => {
             alertContainer.remove();
-        }, 500); // Tempo para garantir que a animação de saída aconteça
+        }, 3000); // Tempo para garantir que a animação de saída aconteça
     }, 3000); // Tempo para o alerta permanecer visível
 }
 
@@ -322,4 +436,10 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
+document.getElementById('cpf').addEventListener('input', formatarCPF);
+document.getElementById('telefone').addEventListener('input', formatarTelefone);
 document.getElementById('dataNascimento').addEventListener('input', formatarDataNascimento);
+document.getElementById('cep').addEventListener('input', function () {
+    formatarCep();
+    buscarCep();
+});
